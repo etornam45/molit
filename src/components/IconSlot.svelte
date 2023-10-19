93a1e66dbead45c1faa9cc2bbad51196b752473c
etornam45 <navidben45@gmail.com>
2023-10-19 07:15:03 +0000

    Added support to toggle Left Sidebar Icons

--- src/components/IconSlot.svelte
+++ src/components/IconSlot.svelte
@@ -1,3 +1,23 @@
-<div class="icons bg-slate-700  hover:bg-purple-500 cursor-pointer flex items-center justify-center w-8 h-8 rounded m-2">
-    <slot></slot>
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+    /**
+	 * @type {string}
+	 */
+     export let _class
+
+	function sayHello() {
+		dispatch('select');
+	}
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+    on:click={sayHello}
+	class="{_class != ""? "bg-slate-600 border border-gray-400 selected": "bg-[transparent]"} icons hover:border hover:border-gray-400 cursor-pointer flex items-center justify-center w-8 h-8 rounded m-2"
+>
+	<slot />
 </div>
--- src/components/LeftSideBar.svelte
+++ src/components/LeftSideBar.svelte
@@ -1,35 +1,53 @@
 <script>
-	import CursorSelect from "../icons/cursor-select.svelte";
-	import PenNib from "../icons/pen-nib.svelte";
-    import PolygonThin from "../icons/polygon-thin.svelte";
-	import Square from "../icons/square.svelte";
-	import TextIcon from "../icons/text-icon.svelte";
-	import Triangle from "../icons/triangle.svelte";
-	import IconSlot from "./IconSlot.svelte";
+	import CursorSelect from '../icons/cursor-select.svelte';
+	import PenNib from '../icons/pen-nib.svelte';
+	import PolygonThin from '../icons/polygon-thin.svelte';
+	import Square from '../icons/square.svelte';
+	import TextIcon from '../icons/text-icon.svelte';
+	import Triangle from '../icons/triangle.svelte';
+	import IconSlot from './IconSlot.svelte';
+
+	import { selectedCursor } from '$lib/stores';
+	import Cursor from '../icons/cursor.svelte';
+
+	/**
+	 * @type {string | undefined}
+	 */
+	let _selectedCursor;
+
+	selectedCursor.subscribe((value) => {
+		_selectedCursor = value;
+	});
+	/**
+	 *
+	 * @param Cursor {string}
+	 */
+	function updateSelectedCursor(Cursor) {
+		selectedCursor.set(Cursor);
+	}
 
 </script>
-<aside class="h-full bg border-r b-color w-fit ">
 
-    <!-- Cursors and shapes -->
-    <div class="cursors">
-        <IconSlot >
-            <CursorSelect />
-        </IconSlot>
-        <IconSlot>
-            <PenNib />
-        </IconSlot>
-        <IconSlot>
-            <Square/>
-        </IconSlot>
-        <IconSlot>
-            <Triangle />
-        </IconSlot>
-        <IconSlot> 
-            <PolygonThin />
-        </IconSlot>
-        <IconSlot>
-            <TextIcon />
-        </IconSlot>
-    </div>
-
+<aside class="h-full bg border-r b-color w-fit">
+	<!-- Cursors and shapes -->
+	<div class="cursors">
+		<IconSlot on:select={() => updateSelectedCursor('Pointer')} _class={_selectedCursor == "Pointer"? _selectedCursor: ""}>
+			<CursorSelect />
+		</IconSlot>
+		<IconSlot on:select={() => updateSelectedCursor('Pen')} _class={_selectedCursor == "Pen"? _selectedCursor: ""}>
+			<PenNib />
+		</IconSlot>
+		<IconSlot on:select={() => updateSelectedCursor('Square')} _class={_selectedCursor == "Square"? _selectedCursor: ""}>
+			<Square />
+		</IconSlot>
+		<IconSlot on:select={() => updateSelectedCursor('Triangle')} _class={_selectedCursor == "Triangle"? _selectedCursor: ""}>
+			<Triangle />
+		</IconSlot>
+		<IconSlot on:select={() => updateSelectedCursor('Polygon')} _class={_selectedCursor == "Polygon"? _selectedCursor: ""}>
+			<PolygonThin />
+		</IconSlot>
+		<IconSlot on:select={() => updateSelectedCursor('Text')} _class={_selectedCursor == "Text"? _selectedCursor: ""}>
+			<TextIcon />
+		</IconSlot>
+	</div>
 </aside>
