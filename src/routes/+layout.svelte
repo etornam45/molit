0ce1cf9069d2da997b76d0533c379d8d8ff9046a
etornam45 <navidben45@gmail.com>
2023-10-19 07:18:15 +0000

    Removed unecessary writable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
-	import { writable } from 'svelte/store';
+
 	import '../app.css';
-	import { getContext, setContext } from 'svelte';
 
 	/** @type {import('./$types').LayoutData} */
 	// export let data;
 	
-	let user_cursor = writable()
-	$: user_cursor.set({
-		x: 0,
-		y: 0
-	})
-
-	setContext('user_cursor', user_cursor);
 </script>
 
 <slot />
