031c05676cbf357daa8814116b04f95578fd559c
etornam45 <navidben45@gmail.com>
2023-10-28 20:13:49 +0000

    Added Right SideBar

--- src/components/RightSideBar.svelte
+++ src/components/RightSideBar.svelte
@@ -1,3 +1,14 @@
-<aside class="w-[300px] bg h-full border-l b-color">
+<script>
+	import SegmentSlote from "./RightSideBar/SegmentSlote.svelte";
 
+</script>
+<aside class="w-[300px] bg h-full border-l b-color">
+    <SegmentSlote title="Properties">
+        <div class="h-[500px]">
+
+        </div>
+    </SegmentSlote>
+    <SegmentSlote title="Layers">
+        Hello
+    </SegmentSlote>
 </aside>
