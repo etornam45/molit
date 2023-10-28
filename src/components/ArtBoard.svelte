c3db2e93999e22fd0dafd7a33db9b62cc5a8c604
etornam45 <navidben45@gmail.com>
2023-10-28 20:16:23 +0000

    Modefied ArtBoard + events + drawing staight paths

--- src/components/ArtBoard.svelte
+++ src/components/ArtBoard.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
-	import { socket } from '$lib/websocket';
 	import { onMount } from 'svelte';
-	import Cursor from '../icons/cursor.svelte';
-	import { cursorPosition } from '$lib/stores';
+	import { cursorPosition, selectedCursor } from '$lib/stores';
+	import { generateKey } from '$lib';
+	import { generatePenPath } from '$lib/functions';
 
+	const cursor_pointer = { x: Number(), y: Number() };
 
-	const cursor_pointer = {
-		x: Number(),
-		y: Number()
-	};
-
-	let mouse_cliked = false;
-	let art_board_cursor = {
-		x: 0,
-		y: 0
-	};
-
-	let otheer_cursor = {
-		x: 0,
-		y: 0
-	};
+	let mouse_cliked: boolean;
+	let art_board_cursor: Point = { x: 0, y: 0 };
+	let current_shape_id: string;
+	let otheer_cursor = { x: 0, y: 0 };
 
 	let art_board_properties: DOMRect;
-	let shapes = [];
+
+	interface Point {
+		x: number;
+		y: number;
+	}
+
+	interface Shape {
+		type: string;
+		points: Point[];
+	}
+
+	let shapes: Map<string, Shape> = new Map();
+	let shapesDraw: any = [];
 
 	let art_board: HTMLElement;
 	let main_board: HTMLElement;
@@ -34,21 +36,61 @@
 			main_board = document.querySelector('.art-board') as HTMLElement;
 		};
 	});
+
 	$: if (art_board) {
 		art_board_properties = art_board.getBoundingClientRect();
-		console.log(art_board_properties);
-	}
-	$: if (main_board) {
-		console.log(main_board.scrollHeight, main_board.scrollTop);
 	}
 
-	const hundle_mouse_up = (event: MouseEvent) => {
-		mouse_cliked = false;
-	};
-
+	$: {
+		shapesDraw = [];
+		shapes.forEach(function (value, key) {
+			shapesDraw.push({
+				id: key != undefined ? key : generateKey(),
+				...value
+			});
+		});
+		console.log(shapesDraw);
+	}
 	const hundle_mouse_down = (event: MouseEvent) => {
-		mouse_cliked = true;
-		set_art_board_cursor(event.pageX, event.pageY);
+		// Check if the mouse mouse button is left
+		console.log('clicked', event.button, event.buttons);
+		if (event.button == 0) {
+			if (mouse_cliked == false) {
+				current_shape_id = generateKey();
+			}
+			mouse_cliked = true;
+			set_art_board_cursor(event.pageX, event.pageY);
+
+			switch ($selectedCursor) {
+				case 'Pen':
+					if (!shapes.has(current_shape_id)) {
+						shapes.set(current_shape_id, {
+							type: 'Pen',
+							points: [{ x: art_board_cursor.x, y: art_board_cursor.y }]
+						});
+					} else {
+						let _shape = shapes.get(current_shape_id) as Shape;
+						_shape?.points.push({ x: art_board_cursor.x, y: art_board_cursor.y });
+						shapes.set(current_shape_id, _shape);
+						shapes = shapes;
+					}
+					console.log(shapes.size);
+					break;
+				case 'Pointer':
+					break;
+				case 'Triangle':
+					break;
+				case 'Square':
+					break;
+				default:
+					// Do nothing
+					break;
+			}
+			shapes.forEach(function (value, key) {
+				console.log(key);
+				console.table(value.points);
+			});
+		}
 	};
 
 	const hundle_mouse_move = (event: MouseEvent) => {
@@ -59,19 +101,56 @@
 
 		if (mouse_cliked) {
 			set_art_board_cursor(event.pageX, event.pageY);
+
+			switch ($selectedCursor) {
+				case 'Pen':
+					// Handle the bezier move here.
+					break;
+				case 'Pointer':
+					break;
+				case 'Triangle':
+					break;
+				case 'Square':
+					break;
+				default:
+					// Do nothing
+					break;
+			}
+			// console.log(shapes);
+		}
+	};
+
+	const hundle_mouse_up = (event: MouseEvent) => {
+		switch ($selectedCursor) {
+			case 'Pen':
+				if (event.button == 1) {
+					// Release pen when mouse wheel is clicked
+					console.log('Pen mode selected');
+					mouse_cliked = false;
+				} else {
+					console.log(event.button);
+				}
+				break;
+			case 'Pointer':
+				mouse_cliked = false;
+				break;
+			case 'Triangle':
+				break;
+			case 'Square':
+				break;
+			default:
+				// Do nothing
+				break;
 		}
 	};
 
 	const set_art_board_cursor = (pageX: number, pageY: number) => {
-		art_board_cursor.x = pageX - art_board_properties.x + main_board.scrollLeft;
-		art_board_cursor.y = pageY - art_board_properties.y + main_board.scrollTop;
+		(art_board_cursor.x = pageX - art_board_properties.x + main_board.scrollLeft),
+			(art_board_cursor.y = pageY - art_board_properties.y + main_board.scrollTop);
 	};
-
 	const set_user_cursor_position = (pageX: number, pageY: number) => {
-		cursor_pointer.x = pageX;
-		cursor_pointer.y = pageY;
-
-		cursorPosition.set({x: cursor_pointer.x, y: cursor_pointer.y});
+		(cursor_pointer.x = pageX), (cursor_pointer.y = pageY);
+		cursorPosition.set({ x: cursor_pointer.x, y: cursor_pointer.y });
 	};
 </script>
 
@@ -87,14 +166,19 @@
 		class="bg-white h-[800px] w-[1300px] relative top-[50%] translate-y-[-50%]"
 		style="margin: 0 auto;"
 	>
-		<circle cx={art_board_cursor.x} cy={art_board_cursor.y} r="15" fill="none" stroke="indigo"/>
+		<circle cx={art_board_cursor.x} cy={art_board_cursor.y} r="15" fill="none" stroke="indigo" />
+		{#each shapesDraw as { id, type, points } (id)}
+			{#if type == 'Pen'}
+				<path d={generatePenPath(points)} stroke='black' fill='none'/>
+			{/if}
+		{/each}
 	</svg>
 </main>
 
 <style>
 	.art-board::-webkit-scrollbar {
-		width: 8px;
-		height: 8px;
+		width: 0px;
+		height: 0px;
 		background: rgb(35, 35, 37);
 		appearance: none;
 	}
@@ -104,8 +188,9 @@
 		cursor: grab;
 	}
 
-	.art-board::-webkit-scrollbar-track{
-		width: 10px;height: 10px;
+	.art-board::-webkit-scrollbar-track {
+		width: 10px;
+		height: 10px;
 		/* background: red; */
 	}
 </style>
