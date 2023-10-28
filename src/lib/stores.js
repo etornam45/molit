a8f494402c8373815ec6fda9d1d21365d3da2e77
etornam45 <navidben45@gmail.com>
2023-10-28 20:14:39 +0000

    modified stores

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,6 +1,7 @@
 import { writable } from "svelte/store";
 
 
-export const selectedCursor = writable("Triangle");
+
+export const selectedCursor = writable("Pointer");
 
 export const cursorPosition = writable({x: 0, y: 0})
