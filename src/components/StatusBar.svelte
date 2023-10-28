d48f91f5fd25a88649f23f1eff0936da5befa844
etornam45 <navidben45@gmail.com>
2023-10-28 20:14:02 +0000

    Added Status Bar

--- src/components/StatusBar.svelte
+++ src/components/StatusBar.svelte
@@ -1,33 +1,15 @@
 <script>
     import { cursorPosition, selectedCursor } from "$lib/stores";
-
-    /**
-	 * @type {any}
-	 */
-    let cursorValue;
-    /**
-	 * @type {{ x: number; y: number; }}
-	 */
-    let _cursor_position;
-
-    selectedCursor.subscribe(value => {
-        cursorValue = value;
-    })
-
-    cursorPosition.subscribe(value => {
-        _cursor_position = value;
-    })
-
 </script>
 
 <footer class="bg-emerald-400 text-sm border-t b-color  flex gap-3 px-2">
     <!-- Selected shape -->
-    <div class="">
-        <b>Cursor:</b> {cursorValue}
+    <div class="w-[120px]">
+        <b>Cursor : </b> {$selectedCursor}
     </div>
 
     <div class=" bg-emerald-800 px-2 text-white/70">
-        <b>X:</b> {_cursor_position.x}
-        <b>, Y:</b> {_cursor_position.y} 
+        <b>X : </b> {$cursorPosition.x}
+        <b>, Y : </b> {$cursorPosition.y} 
     </div>
 </footer>
