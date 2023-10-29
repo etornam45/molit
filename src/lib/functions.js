3f812587ee7d6e32aa294e1ec9912b6080d12444
etornam45 <navidben45@gmail.com>
2023-10-29 00:02:44 +0000

    Added functions to generate path for Squre and Triangle

--- src/lib/functions.js
+++ src/lib/functions.js
@@ -23,4 +23,55 @@ export function generatePenPath(Points) {
 	return d.trim() + 'z';
 }
 
+/**
+ * @param {[{
+ *          x: number,
+ *          y: number
+ *      }]} Points
+ * @returns {string}
+ */
+export function generateSquarePath(Points) {
+	let d = '';
 
+	if (Points.length > 0) {
+		// @ts-ignore
+		d = `M ${Points[0].x} ${Points[0].y} L${Points[1].x} ${Points[0].y} L${Points[1].x} ${Points[1].y} L${Points[0].x} ${Points[1].y}`;
+		return d.trim() + 'z';
+	}
+	return '';
+}
+
+/**
+ * @param {[
+ * 			{
+ *          	x: number,
+ *          	y: number
+ *      	}
+ * 		]} Points
+ * @returns {string}
+ */
+export function generateTrianglePath(Points) {
+	let d = '';
+
+	if (Points.length > 0) {
+		// @ts-ignore
+		d = `M ${(Points[0].x + Points[1].x)/2} ${Points[0].y} L${Points[1].x} ${Points[1].y} L${Points[0].x} ${Points[1].y}`;
+		return d.trim() + 'z';
+	}
+	return '';
+}
+
+/**
+ * @returns {string}
+ */
+export function generateColor() {
+	return `rgb(${randomNumber_255()},${randomNumber_255()},${randomNumber_255()})`;
+}
+
+/**
+ *
+ * @returns {number}
+ */
+function randomNumber_255() {
+	return Math.floor(Math.random() * 255);
+}
