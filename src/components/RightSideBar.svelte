296551e9710994ca1a7f25270165d039a348981e
etornam45 <navidben45@gmail.com>
2023-10-29 00:02:10 +0000

    Store listening to selected shape

--- src/components/RightSideBar.svelte
+++ src/components/RightSideBar.svelte
@@ -1,6 +1,6 @@
 <script>
 	import SegmentSlote from "./RightSideBar/SegmentSlote.svelte";
-
+    import {selectedShape} from "$lib/stores"
 </script>
 <aside class="w-[300px] bg h-full border-l b-color">
     <SegmentSlote title="Properties">
@@ -9,6 +9,6 @@
         </div>
     </SegmentSlote>
     <SegmentSlote title="Layers">
-        Hello
+        {$selectedShape}
     </SegmentSlote>
 </aside>
