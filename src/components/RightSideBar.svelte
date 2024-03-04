fe1aa3bf5ff72cb5ad585fc1f1c0a110d044279f
etornam45 <navidben45@gmail.com>
2024-03-04 14:18:46 +0000

    Added Layers to the RighSideBar

--- src/components/RightSideBar.svelte
+++ src/components/RightSideBar.svelte
@@ -1,14 +1,27 @@
 <script>
-	import SegmentSlote from "./RightSideBar/SegmentSlote.svelte";
-    import {selectedShape} from "$lib/stores"
+	import { selectedShape, shapes, shapesDrawn } from '$lib/stores';
+	import SegmentSlote from './RightSideBar/SegmentSlote.svelte';
+	$: activeShape = $shapes.get($selectedShape);
+	$: console.log($selectedShape);
 </script>
-<aside class="w-[300px] bg h-full border-l b-color">
-    <SegmentSlote title="Properties">
-        <div class="h-[500px]">
 
-        </div>
-    </SegmentSlote>
-    <SegmentSlote title="Layers">
-        {$selectedShape}
-    </SegmentSlote>
+<aside class="w-[300px] bg h-full border-l b-color">
+	{#if $selectedShape}
+		<SegmentSlote title="Properties">
+			<div class="h-[500px]">
+				{activeShape?.preferences.background}
+			</div>
+		</SegmentSlote>
+	{/if}
+	<SegmentSlote title="Layers">
+		{#each $shapesDrawn as shapes}
+			<p  class="cursor-pointer user-select-none"
+				on:click={() => {
+					selectedShape.set(shapes.id);
+				}}
+			>
+				{shapes.type}
+			</p>
+		{/each}
+	</SegmentSlote>
 </aside>
